ffca6cc3a6d1a13e0be389a45379ef42e07090b1
Elsonloc <elsonloc@gmail.com>
2025-03-04 01:27:09 +0000

    DESAFIO NOVATO T2 CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,25 +8,24 @@
 
 
 
-    int main()
-    {
-         // Definindo as variáveis
+    int main(){
+
+   // Definindo as variáveis.
     // Carta 1
     int carta = 1;
-    char Estado[20] = "Pará"; // Evitar usar nomes fixos para depois conseguir mudar atraves de algumas variáveis
+    char Estado[20] = "Pará";         // Evitar usar nomes fixos para depois conseguir mudar atraves de algumas variáveis
     char Codigo_da_Carta[20] = "A01"; // O código da carta é uma string.
     char Cidade[20] = "Belem";
     unsigned long int Populacao = 8120000;
-    float Area_em_km = 1.248000;// A área está em km².
-    float pib = 262.905;// PIB referenciado em Milhoes de Reais.
-    int pontos = 135;// Número de pontos turísticos pode variar de cidade para cidade.
+    float Area_em_km = 1.248000;  // A área está em km².
+    float pib = 262.905;          // PIB referenciado em Milhoes de Reais.
+    int pontos = 135;             // Número de pontos turísticos pode variar de cidade para cidade.
     float Densidade_populacional; // Variável para armazenar a densidade populacional
-    float Pib_per_Capta;// Variável para armazenar o PIB per capita
-    float Super_Poder; // Variável para armazenar o Super Poder
- 
+    float Pib_per_Capta;          // Variável para armazenar o PIB per capita
+    float Super_Poder;            // Variável para armazenar o Super Poder
 
-    //As informaçoes da carta 1 tambem serao usadas para a carta 2 com pequenas modificaçoes
-    // Carta 2
+    // As informaçoes da carta 1 tambem serao usadas para a carta 2 com pequenas modificaçoes
+    //  Carta 2
     int carta2 = 2;
     char Estado2[20] = "Pernambuco";
     char Codigo_da_Carta2[20] = "B02";
@@ -51,46 +50,97 @@
 
     // Exibindo os resultados das comparações
     printf(" Comparação de Cartas\n");
-
+    // Foram adicionados parametros de ESTRUTURA DE DECISÃO para melhorar a visualização do jogo
     // Comparando População
-
     printf("Populacao: ");
+    if (Populacao > Populacao2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (Populacao > Populacao2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
 
     // Comparando Área
-
     printf("Area: ");
+    if (Area_em_km > Area_em_km2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (Area_em_km > Area_em_km2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
 
     // Comparando PIB
-
     printf("PIB: ");
+    if (pib > pib2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (pib > pib2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
 
     // Comparando Pontos Turísticos
-
     printf("Pontos Turisticos: ");
+    if (pontos > pontos2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (pontos > pontos2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
 
     // Comparando PIB per Capita
-
     printf("PIB per Capita: ");
+    if (Pib_per_Capta > Pib_per_Capta2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (Pib_per_Capta > Pib_per_Capta2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
 
     // Comparando Densidade Populacional (menor densidade vence)
-
     printf("Densidade Populacional: ");
+    if (Densidade_populacional > Densidade_populacional2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (Densidade_populacional < Densidade_populacional2) ? 1 : 0); // 1 para Carta 1 vence (menor densidade), 0 para Carta 2 vence
 
     // Comparando Super Poder
-
     printf("Super Poder: ");
+    if (Super_Poder > Super_Poder2)
+    {
+        printf("Carta 1 vence - ");
+    }
+    else
+    {
+        printf("Carta 2 vence - ");
+    }
+
     printf("%d\n", (Super_Poder > Super_Poder2) ? 1 : 0); // 1 para Carta 1 vence, 0 para Carta 2 vence
-
-    
-        
-    
-
 
     return 0; // Encerramento da função main (Sempre encerrar)
 
